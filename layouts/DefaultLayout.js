e06980c22bd77cba601425f20128bc40ffdb7cd9
Eitaro Fukamachi <e.arrows@gmail.com>
2021-07-21 20:06:45 +0900

    Tweak margin/paddings.

--- layouts/DefaultLayout.js
+++ layouts/DefaultLayout.js
@@ -4,7 +4,7 @@ import GlobalHeader from '../molecules/GlobalHeader'
 import GlobalFooter from '../molecules/GlobalFooter'
 
 const MainContent = (props) => (
-  <Box px={[8, 10, 10, 50]} py={[8, 8, 8, '35px']} maxW="1200px" mx="auto" {...props} />
+  <Box px={[6, 10, 10, 50]} py={[8, 8, 8, '35px']} maxW="1200px" mx="auto" {...props} />
 )
 
 export default function DefaultLayout({ title, description, globalHeader, children }) {
--- molecules/GlobalFooter.js
+++ molecules/GlobalFooter.js
@@ -9,7 +9,7 @@ const styles = {
 
 const GlobalFooter = (props) => (
   <footer>
-    <Wrap py={[10, 10, 50]} mt={50} justify="center" spacing={10} {...styles}>
+    <Wrap py={[10, 10, 50]} mt={30} justify="center" spacing={[6, 6, 10]} {...styles}>
       <WrapItem><Link href="https://github.com/quickdocs">GitHub</Link></WrapItem>
       <WrapItem><Link href="https://www.quicklisp.org/beta/">Quicklisp</Link></WrapItem>
       <WrapItem><Link href="https://github.com/sponsors/fukamachi">Sponsor</Link></WrapItem>
