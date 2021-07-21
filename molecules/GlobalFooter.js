89010eba9b2054cb098fb42dc6ba8a89d2e4c3ad
Eitaro Fukamachi <e.arrows@gmail.com>
2021-07-21 17:23:41 +0900

    Add links at the global footer.

--- molecules/GlobalFooter.js
+++ molecules/GlobalFooter.js
@@ -9,8 +9,10 @@ const styles = {
 
 const GlobalFooter = (props) => (
   <footer>
-    <Wrap py={50} mt={50} justify="center" spacing={10} {...styles}>
-      <WrapItem><Link href="https://github.com/quickdocs">GitHub @quickdocs</Link></WrapItem>
+    <Wrap py={[10, 10, 50]} mt={50} justify="center" spacing={10} {...styles}>
+      <WrapItem><Link href="https://github.com/quickdocs">GitHub</Link></WrapItem>
+      <WrapItem><Link href="https://www.quicklisp.org/beta/">Quicklisp</Link></WrapItem>
+      <WrapItem><Link href="https://github.com/sponsors/fukamachi">Sponsor</Link></WrapItem>
     </Wrap>
   </footer>
 )
