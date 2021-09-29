26ffd26d2310d66ec5b8a6c239f811dcc387335d
Eitaro Fukamachi <e.arrows@gmail.com>
2021-09-29 11:18:40 +0900

    Remove the sponsor link at the bottom.

--- molecules/GlobalFooter.js
+++ molecules/GlobalFooter.js
@@ -12,7 +12,6 @@ const GlobalFooter = (props) => (
     <Wrap py={[10, 10, 50]} mt={30} justify="center" spacing={[6, 6, 10]} {...styles}>
       <WrapItem><Link href="https://github.com/quickdocs">GitHub</Link></WrapItem>
       <WrapItem><Link href="https://www.quicklisp.org/beta/">Quicklisp</Link></WrapItem>
-      <WrapItem><Link href="https://github.com/sponsors/fukamachi">Sponsor</Link></WrapItem>
     </Wrap>
   </footer>
 )
